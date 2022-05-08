a418dce74e7d129c9c3daa1ce6857ecdaebe56f5
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-08 16:19:12 -0700

    build(next): add import transform to reduce bundle size

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    modularizeImports: {
+      'lodash/fp': {
+        transform: 'lodash/fp/{{ member }}'
+      },
+      '@mui/material': {
+        transform: '@mui/material/{{ member }}'
+      },
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{ member }}'
+      },
+    }
+  }
 };
 
 export default nextConfig;

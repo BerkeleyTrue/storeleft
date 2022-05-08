b6c53c789593ff8d97b02ec11735ee9d165df020
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-07 22:56:37 -0700

    feat(client): add list

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,24 +1,29 @@
 import { useBoolean } from '@chakra-ui/hooks';
 import {
   AppBar,
+  Container,
+  Grid,
   IconButton,
+  Stack,
   Toolbar,
   Typography,
 } from '@mui/material';
 import { Box } from '@mui/system';
 import { Menu } from '@mui/icons-material';
+
 import { AppDrawer } from './Drawer';
+import { List } from './Items/List';
 
 function App() {
   const [isOpen, setOpen] = useBoolean(false);
   return (
-    <Box>
+    <Box width='100vw' height='100vh'>
       <AppBar>
         <Toolbar>
           <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
+            edge='start'
+            color='inherit'
+            aria-label='open drawer'
             onClick={setOpen.toggle}
             sx={{
               marginRight: '36px',
@@ -30,7 +35,16 @@ function App() {
           <Typography>StoreLeft</Typography>
         </Toolbar>
       </AppBar>
-      <AppDrawer isOpen={isOpen} onMenuClick={setOpen.toggle}/>
+      <AppDrawer isOpen={isOpen} onMenuClick={setOpen.toggle} />
+      <Toolbar />
+      <Stack height='100%' width='100%' overflow-x='hidden'>
+        <Box height='4em'>
+          <Typography>Items</Typography>
+        </Box>
+        <Box flexGrow='1'>
+          <List />
+        </Box>
+      </Stack>
     </Box>
   );
 }
---
+++ src/client/Items/List.tsx
@@ -0,0 +1,41 @@
+import _ from 'lodash/fp';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Box } from '@mui/system';
+
+const getRowId = _.get('_id');
+
+const data = [
+  {
+    _id: '8822aa2c-58e9-479f-8439-6157e4b2c522',
+    _rev: '3-4186fe5f6d22184fb90902382de5bd3f',
+    status: {
+      checkInDate: '2022-05-07T06:43:13.735Z',
+      checkInBy: 'Local',
+      status: true,
+    },
+    location: 'test/shelf',
+    name: 'Test Foo',
+    type: 'item',
+    lastModified: '2022-05-07T06:49:48.901Z',
+    lastTouchDate: '2022-05-07T06:43:13.735Z',
+  },
+];
+
+const columns: GridColDef[] = [
+  { headerName: 'Id', field: '_id' },
+  { headerName: 'Name', field: 'name', type: 'string' },
+];
+
+export const List = () => {
+  return (
+    <Box height='500px' width='100%'>
+      <DataGrid
+        rows={data}
+        getRowId={getRowId}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[10, 50, 100, 1000]}
+      />
+    </Box>
+  );
+};

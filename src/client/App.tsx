a36964392820ff5f23b59142f8e79725c9af73e7
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-07 17:47:10 -0700

    feat(app): nav

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,29 @@
+import { useBoolean } from "@chakra-ui/hooks";
+import { AppBar, IconButton, Toolbar } from "@mui/material"
+import { Box } from "@mui/system"
+import { Menu } from '@mui/icons-material';
 
 function App() {
+  const [isOpen, setOpen] = useBoolean(false);
   return (
-    <div>
-      <header>
-        FOo
-      </header>
-    </div>
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={setOpen.toggle}
+              sx={{
+                marginRight: '36px',
+                ...(isOpen && { display: 'none' })
+              }}
+            >
+              <Menu />
+            </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 

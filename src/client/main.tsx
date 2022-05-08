c3c30eebe2fae9e64c6c03d40607cdeb2082d9de
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-07 17:30:07 -0700

    feat(app): add mui

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
 import App from './App';
 import './index.css';
+import theme from './theme';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const element = document.getElementById('root');
+
+ReactDOM.createRoot(element!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
     <App />
   </React.StrictMode>
 );

e502a3be814e0f0e6e5ea913dc689ddf7d0519db
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-09 21:31:18 -0700

    fix(lib/pouchdb): add alldocs options

--- src/lib/pouchdb/useAllDocs.ts
+++ src/lib/pouchdb/useAllDocs.ts
@@ -2,24 +2,18 @@ import { useCallback } from 'react';
 import useSWR, { SWRResponse } from 'swr';
 import { usePouchDbContext } from './useContext';
 
-export interface AllDocsQuery {
-  limit: number;
-  skip: number;
-  include_docs: boolean;
-}
-
 export interface UseAllDocsOptions {
-  query: AllDocsQuery;
+  query: PouchDB.Core.AllDocsOptions;
 }
 
 export const useAllDocs = ({
-  query: { limit, skip, include_docs },
+  query: { limit, skip, include_docs, ...rest },
 }: UseAllDocsOptions): SWRResponse<PouchDB.Core.AllDocsResponse<{}>> => {
 
   const { db } = usePouchDbContext();
 
   const allDocsFetcher = useCallback(() => {
-    return db.allDocs({ skip, limit, include_docs });
+    return db.allDocs({ skip, limit, include_docs, ...rest });
   }, [db]);
 
   return useSWR(['allDocs', limit, skip, include_docs], allDocsFetcher);

dfe9dabaa4d21a69da774a7c2ec829b4023db613
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-10 19:38:03 -0700

    fix(ItemList): base table

--- src/components/Items/List.tsx
+++ src/components/Items/List.tsx
@@ -1,7 +1,20 @@
 import _ from 'lodash/fp';
 import { useCallback } from 'react';
 import { useRouter } from 'next/router';
-import { Heading, Box } from '@chakra-ui/react';
+import {
+  Heading,
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  VStack,
+  TableCaption,
+} from '@chakra-ui/react';
 
 import { useAllDocs } from '../../lib/pouchdb/useAllDocs';
 import { useItemHeaders } from '../../services/config/use-headers';
@@ -29,8 +42,44 @@ export const ItemList = () => {
   )(data);
 
   return (
-    <Box height='500px' width='100%' px='1em'>
-      Foo
-    </Box>
+    <VStack>
+      <Heading>Items</Heading>
+      <TableContainer>
+        <Table variant='simple'>
+          <TableCaption>Imperial to metric conversion factors</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>To convert</Th>
+              <Th>into</Th>
+              <Th isNumeric>multiply by</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>inches</Td>
+              <Td>millimetres (mm)</Td>
+              <Td isNumeric>25.4</Td>
+            </Tr>
+            <Tr>
+              <Td>feet</Td>
+              <Td>centimetres (cm)</Td>
+              <Td isNumeric>30.48</Td>
+            </Tr>
+            <Tr>
+              <Td>yards</Td>
+              <Td>metres (m)</Td>
+              <Td isNumeric>0.91444</Td>
+            </Tr>
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th>To convert</Th>
+              <Th>into</Th>
+              <Th isNumeric>multiply by</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+    </VStack>
   );
 };

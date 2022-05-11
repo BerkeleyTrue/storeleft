042c1825e08ab2bac418e7c60fb058f19fa9d8da
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-11 12:57:13 -0700

    feat(layout): drawer and content contained within vw

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,22 +1,46 @@
-import { PropsWithChildren } from 'react';
+import { ComponentProps, PropsWithChildren } from 'react';
 import { useBoolean } from '@chakra-ui/hooks';
 import { Flex, Box, useBreakpointValue } from '@chakra-ui/react';
 
 import { AppDrawer, TNavigation } from './Drawer';
 import { AppBar } from './AppBar';
 
+type BoxWidth = ComponentProps<typeof Box>['width'];
 interface Variant {
-  navigation: TNavigation
+  navigation: TNavigation;
   showButton: boolean;
+  sidebarWidth?: BoxWidth;
+  contentWidth: BoxWidth;
 }
 
+const getContentWidth = (size: number | string): string =>
+  `calc(100% - var(--chakra-sizes-${size}))`;
+
 const headerSize = 12;
-const smVariant: Variant = { navigation: 'drawer', showButton: true };
-const mdVariant: Variant = { navigation: 'sidebar', showButton: false };
+
+const sidebarWidth: BoxWidth = { base: '64', xl: '80', '2xl': '96' };
+
+const smVariant: Variant = {
+  navigation: 'drawer',
+  showButton: true,
+  contentWidth: '100%',
+};
+const mdVariant: Variant = {
+  navigation: 'sidebar',
+  showButton: false,
+  sidebarWidth,
+  contentWidth: {
+    base: getContentWidth(sidebarWidth.base as string),
+    xl: getContentWidth(sidebarWidth.base as string),
+    '2xl': getContentWidth(sidebarWidth.base as string),
+  },
+};
 
 export const Layout = ({ children }: PropsWithChildren<{}>) => {
   const [isSidebarOpen, setSidebar] = useBoolean(false);
-  const variants = useBreakpointValue<Variant>({ base: smVariant, md: mdVariant }) || smVariant;
+  const variants =
+    useBreakpointValue<Variant>({ base: smVariant, md: mdVariant }) ||
+    smVariant;
 
   return (
     <Flex width='100vw' height='100vh'>
@@ -25,15 +49,15 @@ export const Layout = ({ children }: PropsWithChildren<{}>) => {
         onMenuClick={setSidebar.toggle}
         onClose={setSidebar.off}
         varient={variants.navigation}
-        sidebarWidth={{ base: '64', xl: '80', '2xl': '96' }}
+        sidebarWidth={variants.sidebarWidth}
       />
 
       <Flex
         flexGrow={1}
-        h="100%"
-        maxH="100vh"
-        w="100%"
-        flexDir="column"
+        h='100%'
+        maxH='100vh'
+        maxW={variants.contentWidth}
+        flexDir='column'
       >
         <AppBar
           h={headerSize}
@@ -43,12 +67,12 @@ export const Layout = ({ children }: PropsWithChildren<{}>) => {
 
         <Flex
           h={`calc(100% - var(--chakra-sizes-${headerSize}))`}
-          w="100%"
-          px="4"
-          overflowX="hidden"
-          overflowY="auto"
+          w='100%'
+          px='4'
+          overflowX='hidden'
+          overflowY='auto'
         >
-          <Box mx="auto" w="100%" maxW="container.xl">
+          <Box mx='auto' w='100%' maxW='container.xl'>
             {children}
           </Box>
         </Flex>

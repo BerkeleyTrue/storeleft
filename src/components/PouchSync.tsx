6ac78ab5db15b60d1353e9c325123d4512685fc4
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-14 20:21:29 -0700

    feat(pouchsync): add live two way sync after intial sync

--- src/components/PouchSync.tsx
+++ src/components/PouchSync.tsx
@@ -1,29 +1,52 @@
 import { useToast } from '@chakra-ui/react';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import { useAppEffect } from '../lib/hooks/use-app-effect';
 
 import { usePouchDbContext } from '../lib/pouchdb/useContext';
 
+const getRemoteUrl = () => `${window.location.origin}/api/db/storedown`;
+
 export const PouchSync = ({ children }: PropsWithChildren<{}>) => {
   const toast = useToast();
   const { db } = usePouchDbContext();
+  const [hasSynced, setHasSynced] = useState(false);
 
   useAppEffect(() => {
-    const remoteUrl = `${window.location.origin}/api/db/storedown`;
+    const remoteUrl = getRemoteUrl();
     toast({
       title: 'Syncing db...',
       status: 'info',
       variant: 'solid',
     });
 
-    db.replicate.from(remoteUrl).on('complete', () => {
+    const syncSubscription = db.replicate.from(remoteUrl).on('complete', () => {
       toast({
         title: 'initial replicate complete.',
         status: 'success',
         variant: 'solid',
       });
+      setHasSynced(true);
     });
+
+    return () => {
+      syncSubscription.cancel();
+    };
   }, [db]);
+
+  useEffect(() => {
+    const remoteUrl = getRemoteUrl();
+    let syncSubscription: PouchDB.Replication.Sync<{}>;
+
+    if (hasSynced) {
+      syncSubscription = db.sync(remoteUrl, { live: true, retry: true });
+    }
+
+    return () => {
+      if (syncSubscription) {
+        syncSubscription.cancel();
+      }
+    };
+  }, [db, hasSynced]);
 
   return <>{children}</>;
 };

958b07db7f32b149b629310cae203230921a213b
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-16 21:26:01 -0700

    feat(lib/pouchdb/useGet): add swr options

--- src/lib/pouchdb/useGet.ts
+++ src/lib/pouchdb/useGet.ts
@@ -1,19 +1,40 @@
 import { useCallback } from 'react';
 import useSWR from 'swr';
+import { PublicConfiguration, BareFetcher } from 'swr/dist/types';
 import { O } from 'ts-toolbelt';
 
 import { usePouchDbContext } from './useContext';
 
-export interface UseGetOptions {
+type FetcherResponse<Model> = Model &
+  PouchDB.Core.IdMeta &
+  PouchDB.Core.GetMeta;
+
+export interface UseGetOptions<Model> {
   query: O.Merge<PouchDB.Core.GetOptions, { docId: string }>;
+  shouldPause?: boolean;
+  swrOptions?: Partial<
+    PublicConfiguration<
+      FetcherResponse<Model>,
+      any,
+      BareFetcher<FetcherResponse<Model>>
+    >
+  >;
 }
 
-export const useGet = <Model>({ query: { docId, ...rest } }: UseGetOptions) => {
+export const useGet = <Model>({
+  shouldPause,
+  query: { docId, ...rest },
+  swrOptions,
+}: UseGetOptions<Model>) => {
   const { db } = usePouchDbContext();
 
   const fetcher = useCallback(() => {
     return db.get<Model>(docId, { ...rest });
   }, [db]);
 
-  return useSWR(['get', docId], fetcher);
+  return useSWR<FetcherResponse<Model>>(
+    shouldPause ? null : ['get', docId],
+    fetcher,
+    swrOptions,
+  );
 };

1114a0f909b346423f7fbf8b517b65b7348fbd9e
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-18 13:23:52 -0700

    build(next): use standalone complex

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'api.ts'],
   reactStrictMode: true,
   experimental: {
+    outputStandalone: true,
     modularizeImports: {
       'lodash/fp': {
         transform: 'lodash/fp/{{ member }}'

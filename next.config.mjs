038633b78afd06bfcdef10aa9d96a57a2a1f15eb
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-18 20:05:07 -0700

    build(docker): don't use standalone complex
    Munges components

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'api.ts'],
   reactStrictMode: true,
   experimental: {
-    outputStandalone: true,
     modularizeImports: {
       'lodash/fp': {
         transform: 'lodash/fp/{{ member }}'

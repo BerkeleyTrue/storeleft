8df769de5797ab5205255061b73bbb30b3c16305
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-18 21:09:12 -0700

    fix(app): disable zoom
    Some weird issue with table page and crazy zoom

--- src/components/AppHead.tsx
+++ src/components/AppHead.tsx
@@ -9,6 +9,10 @@ export const AppHead = ({ subTitle, children }: PropsWithChildren<Props>) => (
   <NextHead>
     <title>StoreLeft//{subTitle}</title>
     <link href='/images/favicon.ico' rel='shortcut icon' />
+    <meta
+      name='viewport'
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+    />
     {children}
   </NextHead>
 );
